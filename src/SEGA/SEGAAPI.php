bf5f2162bd8e4dfef79de0cf841380a2a0a6b929
NightmareXDev <nightmarexbusiness@gmail.com>
2022-04-14 22:52:10 +0100

    Almost done

--- src/SEGA/SEGAAPI.php
+++ src/SEGA/SEGAAPI.php
@@ -2,6 +2,45 @@
 
 namespace SEGA;
 
+use SEGA\Loader;
+use pocketmine\player\Player;
+
 class SEGAAPI {
+
+	public function getKills($player) : ?int { 
+        if(Loader::$database->exists($player)){
+            if($player instanceof Player){
+                $rq = Loader::$database->db->query("SELECT kills FROM players WHERE name = '" . strtolower($player->getName())."'");
+                return $rq->fetchArray()[0]; 
+        }
+      }
+    }
+
+	public function addKill($player) : void {
+        if(Loader::$database->exists($player)){
+            if($player instanceof Player){
+                $newAmount = 1 + $player->getKills();
+                    Loader::$database->db->exec("UPDATE players SET kills = $newAmount WHERE name='".strtolower($player->getName())."'");          
+            } 
+        }
+    }
+
+    public function getDeaths($player) : ?int { 
+        if(Loader::$database->exists($player)){
+            if($player instanceof Player){
+                $rq = Loader::$database->db->query("SELECT deaths FROM players WHERE name = '" . strtolower($player->getName())."'");
+                return $rq->fetchArray()[0]; 
+        }
+      }
+    }
+
+	public function addDeath($player) : void {
+        if(Loader::$database->exists($player)){
+            if($player instanceof Player){
+                $newAmount = 1 + $player->getDeaths();
+                    Loader::$database->db->exec("UPDATE players SET deaths = $newAmount WHERE name='".strtolower($player->getName())."'");          
+            } 
+        }
+    }
 	
 }
--- src/SEGA/SEGAListener.php
+++ src/SEGA/SEGAListener.php
@@ -3,7 +3,13 @@
 namespace SEGA;
 
 use SEGA\Loader;
+use pocketmine\player\Player;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+
+use pocketmine\event\entity\{EntityDamageEvent,
+EntityDamageByEntityEvent};
 
 class SEGAListener implements Listener {
 	
@@ -12,5 +18,33 @@ class SEGAListener implements Listener {
 	public function __construct(Loader $loader) {
 		$this->loader = $loader;
 	}
+
+	public function onPreLogin(PlayerPreLoginEvent $event) : void {
+        
+        $player = $event->getPlayerInfo()->getUsername();
+
+        if(!$this->loader->getDatabase()->exists($player)) {
+            $this->loader->getDatabase()->create($player);
+        } 
+    }
+
+    public function onDeath(PlayerDeathEvent $event){
+
+        $cause = $event->getEntity()->getLastDamageCause();
+
+        if($cause instanceof EntityDamageByEntityEvent) {
+            $killer = $cause->getDamager();
+            $player = $event->getPlayer();
+            if($killer instanceof Player){
+                $this->loader->getAPI()->addDeath($player);
+                $this->loader->getAPI()->addKill($killer);
+            }  
+        }
+
+        if(is_null($cause)){
+            return false;
+        }
+                        
+    }
 
 }
